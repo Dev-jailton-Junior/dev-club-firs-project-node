3ca2db42bf6ef287eebb9c84ef3800a9fd412bb7
Dev-jailton-Junior <junio.jr555@hotmail.com>
2024-02-16 16:08:17 -0300

    melhorias no projeto node

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const uuid = require('uuid')
-const port = 3000
+import express from 'express'
+import {v4} from 'uuid'
+import cors from'cors'
+
+const port = 3001
 const app = express()
 app.use(express.json())
+app.use (cors())
 
 const users = []
 
@@ -21,13 +24,13 @@ const checkUserId = (request, response, next) => {
 }
 
 app.get('/users', (request, response) => {
-    return response.json(users)
-})
+    return response.json(users);
+});
 
 app.post('/users', (request, response) => {
-    const { name, age } = request.body
+    const { name, age } = request.body;
+    const user = { id: v4(), name, age };
 
-    const user = { id: uuid.v4(), name, age }
     users.push(user)
 
     return response.status(201).json(user)
@@ -38,7 +41,7 @@ app.put('/users/:id', checkUserId, (request, response) => {
     const index = request.userIndex
     const id = request.userId
 
-    const updatedUser = { id, name, age }
+    const updatedUser = { id, name, age }  
 
 
     users[index] = updatedUser
@@ -57,6 +60,6 @@ app.delete('/users/:id', (request, response) => {
 
 
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log(`🚀 Server started on port ${port}`)
 })
